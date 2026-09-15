3acf73ab717cc76d185f3e4298c0f61082ee4836
Jessey Holomah <devjessey8@gmail.com>
2026-09-15 23:36:02 +0000

    New Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,12 @@
 // Back-To-Top
 const backToTop = document.getElementById("backToTop");
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        backToTop.classList.add("show");
-    } else {
-        backToTop.classList.remove("show");
-    }
-});
+if (backToTop) {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            backToTop.classList.add("show");
+        } else {
+            backToTop.classList.remove("show");
+        }
+    });
+}
